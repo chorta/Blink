02b00c999c0aad51c67818146153612198c8aae4
Cristian Horta <chorta@ucsd.edu>
2021-06-29 16:31:01 -0700

    updating with UCPA's edits

--- scripts/blink-home.js
+++ scripts/blink-home.js
@@ -69,10 +69,160 @@ function myBlinkLogout() {
 }
 
 
+//Loop through featured news items and check for expired items
 
+$(".featured-item").each(function (i) {
+
+  //Global Date and Time Format
+  var dateFormat = 'YYYY-MM-DD HH:mm';
+
+  //Get Expiration Date Attribute from HTML
+  var getDate = 0;
+  var getDate = $(this).attr("expiration-date");
+
+  //Ensure HTML Date Uses Moment Format 
+  var expirationDate = moment(getDate, 'YYYY-MM-DD HH:mm');
+  
+  //Today's Date and Time
+  var now = moment();
+    
+  //console.log(expirationDate, todayIs);
+
+  var dateDiff = expirationDate.isBefore(now);
+
+  //console.log("Date Expiration is " + expirationDate.format(dateFormat) +" - "+now.format(dateFormat)+ " - "+ dateDiff +" - ");
+  
+
+  //Remove item if item is expired
+  
+    if ( dateDiff === true ) {
+        $(this).remove();
+    }
+
+});
+
+//Loop news items after removing expired items
+$(".featured-news > .featured-item").each(function (j) {
+  
+	//console.log("For each news item ",j++);
+  
+	var totalItems = j++;
+
+	if (totalItems > 0 ) {
+    $(this).remove();
+	}
+
+  
+
+  });
+
+//Loop through news items and check for expired items
+
+$(".news-item").each(function (i) {
+
+  //Global Date and Time Format
+  var dateFormat = 'YYYY-MM-DD HH:mm';
+
+  //Get Expiration Date Attribute from HTML
+  var getDate = 0;
+  var getDate = $(this).attr("expiration-date");
+
+  //Ensure HTML Date Uses Moment Format 
+  var expirationDate = moment(getDate, 'YYYY-MM-DD HH:mm');
+  
+  //Today's Date and Time
+  var now = moment();
+  
+  
+  //console.log(expirationDate, todayIs);
+
+  var dateDiff = expirationDate.isBefore(now);
+
+  //console.log("Date Expiration is " + expirationDate.format(dateFormat) +" - "+now.format(dateFormat)+ " - "+ dateDiff +" - ");
+  
+
+  //Remove item if item is expired
+  
+    if ( dateDiff === true ) {
+        $(this).remove();
+    }
+
+});
+
+//Loop news items after removing expired items
+$(".news-col-1 >.news-item").each(function (j) {
+  
+	//console.log("For each news item ",j++);
+  
+	var totalItems = j++;
+
+	if (totalItems > 5 ) {
+    $(this).remove();
+	}
+
+  
+
+  });
+
+
+
+  $(".event").each(function (i) {
+
+    //Global Date and Time Format
+    var dateFormat = 'YYYY-MM-DD HH:mm';
+  
+    //Get Expiration Date Attribute from HTML
+    var getDate = 0;
+    var getDate = $(this).attr("expiration-date");
+  
+    //Ensure HTML Date Uses Moment Format 
+    var expirationDate = moment(getDate, 'YYYY-MM-DD HH:mm');
+    
+    //Today's Date and Time
+    var now = moment();
+    
+    
+    //console.log(expirationDate, todayIs);
+  
+    var dateDiff = expirationDate.isBefore(now);
+  
+    //console.log("Date Expiration is " + expirationDate.format(dateFormat) +" - "+now.format(dateFormat)+ " - "+ dateDiff +" - ");
+    
+  
+    //Remove item if item is expired
+    
+      if ( dateDiff === true ) {
+          $(this).remove();
+      }
+  
+  });
+  
+  //Loop news items after removing expired items
+  $(".events >.event").each(function (j) {
+    
+    //console.log("For each news item ",j++);
+    
+    var totalItems = j++;
+  
+    if (totalItems > 2 ) {
+      $(this).remove();
+    }
+  
+    
+  
+    });
+
+  
+  /* Tools Button Toggle */
+
+  $('#tools-nav-button > .glyphicon').click(function(){
+    $(this).toggleClass('glyphicon-wrench glyphicon-remove rotate');
+  });
 
 // Footer Year
 
 // var footerDate = moment().format('YYYY');
 //
 // $(".footer-copyright-year").append(footerDate);
+
+
